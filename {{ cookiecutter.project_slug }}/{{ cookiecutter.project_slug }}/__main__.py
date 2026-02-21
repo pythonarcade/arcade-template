bae618cfd396cfca95d66adfb61bb7fbee0b72ad
Darren Eberly <darren.eberly@proton.me>
2026-02-21 17:41:31 -0500

    Try moving assets

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/__main__.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/__main__.py
@@ -2,16 +2,18 @@ from pathlib import Path
 
 import arcade
 
-ASSETS_PATH = Path(__file__).parent.parent / "assets"
+ASSETS_PATH = Path(__file__).parent / "assets"
+
 
 class App(arcade.Window):
-    
     def __init__(self):
         super().__init__(800, 600, "Arcade Project")
         arcade.set_background_color(arcade.color.CORNFLOWER_BLUE)
 
         self.robot_texture = arcade.load_texture(ASSETS_PATH / "robot.png")
-        self.robot_sprite = arcade.Sprite(self.robot_texture, center_x=200, center_y=200)
+        self.robot_sprite = arcade.Sprite(
+            self.robot_texture, center_x=200, center_y=200
+        )
         self.sprite_list = arcade.SpriteList()
         self.sprite_list.append(self.robot_sprite)
 
